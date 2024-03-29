a6bb65925ddbd3f264b5b8cad4fc25e0384cb0e4
Hualdev <hucastillo@gmail.com>
2024-03-28 20:36:54 -0500

    Final Version, enjoy it...

--- faq.js
+++ faq.js
@@ -1,13 +1,19 @@
 let image = document.getElementsByClassName('image')
 let description = document.getElementsByClassName('desc')
+let sub = document.getElementsByClassName('sub')
 
 for(let i= 0; i<image.length; i++ ){
     image[i].addEventListener('click', ()=>{
     description[i].classList.toggle('show')
-    for(let j=0; j<image.length; j++){
-        if(j !== i){
-            description[j].classList.add('hide')
-        }
-    }
+    image[i].classList.toggle('less')
+    image[i].classList.toggle('plus')
+    })
+}
+
+for(let i = 0; i < image.length; i++){
+    sub[i].addEventListener('click', ()=>{
+        description[i].classList.toggle('show')
+        image[i].classList.toggle('less')
+        image[i].classList.toggle('plus')
     })
 }
