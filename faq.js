486839d08c8e9197867f8b0acb35157e008468ac
Hualdev <hucastillo@gmail.com>
2024-03-28 20:53:56 -0500

    Final Version 1

--- faq.js
+++ faq.js
@@ -4,16 +4,28 @@ let sub = document.getElementsByClassName('sub')
 
 for(let i= 0; i<image.length; i++ ){
     image[i].addEventListener('click', ()=>{
-    description[i].classList.toggle('show')
-    image[i].classList.toggle('less')
-    image[i].classList.toggle('plus')
+        if(i==0){
+          description[i].classList.toggle('hide')
+          image[i].classList.toggle('plus')
+        }
+        else{
+            description[i].classList.toggle('show')
+            image[i].classList.toggle('less')
+            image[i].classList.toggle('plus')
+        }
     })
 }
 
 for(let i = 0; i < image.length; i++){
     sub[i].addEventListener('click', ()=>{
-        description[i].classList.toggle('show')
-        image[i].classList.toggle('less')
-        image[i].classList.toggle('plus')
+        if(i==0){
+          description[i].classList.toggle('hide')
+          image[i].classList.toggle('plus')
+        }
+        else{
+            description[i].classList.toggle('show')
+            image[i].classList.toggle('less')
+            image[i].classList.toggle('plus')
+        }
     })
 }
